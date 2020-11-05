1a7afa91cd1e3b946c8fab3bf13951b053357c67
Randy Kamradt <randykamradt@gmail.com>
2020-11-05 04:17:15 -0800

    remove css imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
 import soundsFactory from './sounds';
 import Controller from './controller';
 import Screen from './components/screen';
